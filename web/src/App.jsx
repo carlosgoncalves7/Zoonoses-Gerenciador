6d57dce0b893d64960e9e9d030c7afd2866980a4
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-01 02:02:40 -0300

    feat: remover componentes não utilizados e simplificar a estrutura do App

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router'
 
-import Header from './components/Header'
-import Form from './components/Register'
-import Login from './components/Login'
 
 function App() {
   
   return (
     <>
-      <main>
         <Outlet />
-      </main>
-
     </>
   )
 }
