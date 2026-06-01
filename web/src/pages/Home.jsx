3c1c50b76fabc9974a65e3883be6de06b0bd7b37
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-01 02:02:10 -0300

    feat: reformular a página inicial para incluir o componente Aside e remover o Header

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router"
-import Header from "../components/Header"
+
+import Aside from "../components/NavBar/Aside"
+
 export default function Home(){
 
     return(
-        <>
-        <Header />
-        <h1>Pagina Home!</h1>
-        <main><Outlet/></main>
-        </>
+        <main className="w-full min-h-screen flex flex-row">
+        <Aside />
+        <section className="w-full py-20 px-9"><Outlet/></section>
+        </main>
     )
 }
