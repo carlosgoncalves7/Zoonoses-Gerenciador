34d37767022113fe94adb777d2588dfc2d65c61b
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-01 02:00:19 -0300

    feat: adicionar novos componentes de ícones para Email, Senha

---
+++ web/src/components/IconNav/index.jsx
@@ -0,0 +1,256 @@
+import * as React from "react"
+
+export function Email(props) {
+  return (
+    <svg
+      width={30}
+      height={30}
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_48_956)"
+        stroke="#005666"
+        strokeWidth={1.875}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M3.75 6.563h22.5V22.5a.938.938 0 01-.938.938H4.688a.938.938 0 01-.937-.938V6.562z" />
+        <path d="M26.25 6.563L15 16.874 3.75 6.562" />
+      </g>
+      <defs>
+        <clipPath id="clip0_48_956">
+          <path fill="#fff" d="M0 0H30V30H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function Password(props) {
+  return (
+    <svg
+      width={30}
+      height={30}
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_48_943)"
+        stroke="#005666"
+        strokeWidth={1.875}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M4.688 6.563v16.875M13.125 11.25V15M9.56 13.841L13.124 15M10.92 18.034L13.126 15M15.33 18.034L13.124 15M16.691 13.841L13.125 15M24.375 11.25V15M20.81 13.841L24.374 15M22.17 18.034L24.376 15M26.58 18.034L24.374 15M27.941 13.841L24.375 15" />
+      </g>
+      <defs>
+        <clipPath id="clip0_48_943">
+          <path fill="#fff" d="M0 0H30V30H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function Dog(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_49_14)">
+        <path
+          d="M10 15v1.875M11.25 13.75L10 15l-1.25-1.25M8.125 3.75h3.75M8.125 3.75l-4.19-1.231a.625.625 0 00-.765.491L1.887 9.885c-.12.643.703 1.016 1.106.502L8.125 3.75zM11.875 3.75l4.19-1.231a.625.625 0 01.765.491l1.283 6.875c.12.643-.703 1.016-1.106.502L11.875 3.75z"
+          stroke="currentColor"
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M7.188 11.875a.937.937 0 100-1.875.937.937 0 000 1.875zM12.813 11.875a.937.937 0 100-1.875.937.937 0 000 1.875z"
+          fill="currentColor"
+        />
+        <path
+          d="M16.25 9.405v4.97a2.5 2.5 0 01-2.5 2.5h-7.5a2.5 2.5 0 01-2.5-2.5v-4.97"
+          stroke="currentColor"
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_49_14">
+          <path fill="#fff" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function Cat(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g clipPath="url(#clip0_49_26)">
+        <path
+          d="M10 15v2.5"
+          stroke="currentColor"
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M6.563 11.875a.937.937 0 100-1.875.937.937 0 000 1.875zM13.438 11.875a.937.937 0 100-1.875.937.937 0 000 1.875z"
+          fill="#currentColor"
+        />
+        <path
+          d="M10 3.75v3.125M11.25 13.75L10 15l-1.25-1.25M7.5 4.141v2.734M12.5 4.141v2.734"
+          stroke="currentColor"
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5.281 5.281a8.037 8.037 0 019.438 0l1.714-1.972a.625.625 0 011.067.441v6.875c0 3.797-3.36 6.875-7.5 6.875-4.14 0-7.5-3.078-7.5-6.875V3.75a.625.625 0 011.067-.442L5.281 5.28z"
+          stroke="currentColor"
+          strokeWidth={1.25}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_49_26">
+          <path fill="#fff" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function Book(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_51_498)"
+        stroke="currentColor"
+        strokeWidth={1.25}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M3.75 16.875A1.875 1.875 0 015.625 15H16.25V2.5H5.625A1.875 1.875 0 003.75 4.375v12.5zM3.75 16.875v.625H15" />
+      </g>
+      <defs>
+        <clipPath id="clip0_51_498">
+          <path fill="#fff" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function CheckSquare(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_49_48)"
+        stroke="currentColor"
+        strokeWidth={1.25}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M3.125 11.25v-7.5a.625.625 0 01.625-.625h12.5a.625.625 0 01.625.625v12.5a.624.624 0 01-.625.625h-5.625" />
+        <path d="M3.125 15L5 16.875 9.375 12.5" />
+      </g>
+      <defs>
+        <clipPath id="clip0_49_48">
+          <path fill="#fff" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function Hourglass(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_49_54)"
+        stroke="currentColor"
+        strokeWidth={1.25}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M10 10L5.25 6.437a.625.625 0 01-.25-.5V3.126a.625.625 0 01.625-.625h8.75a.625.625 0 01.625.625v2.784a.625.625 0 01-.248.497L10 10zM10 10l-4.75 3.563a.625.625 0 00-.25.5v2.812a.625.625 0 00.625.625h8.75a.624.624 0 00.625-.625v-2.784a.626.626 0 00-.248-.499L10 10z" />
+      </g>
+      <defs>
+        <clipPath id="clip0_49_54">
+          <path fill="#fff" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export function SignOut(props) {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g
+        clipPath="url(#clip0_49_61)"
+        stroke="currentColor"
+        strokeWidth={1.25}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M8.75 3.125h-5v13.75h5M8.75 10h8.75M14.375 6.875L17.5 10l-3.125 3.125" />
+      </g>
+      <defs>
+        <clipPath id="clip0_49_61">
+          <path fill="currentColor" d="M0 0H20V20H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
