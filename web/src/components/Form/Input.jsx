0e1cb1a077249f8ae735bc32d6d54a4f2eacb84f
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-03 15:45:09 -0300

    fix: corrigir valor padrão do tipo no componente Input e ajustar classes de estilo

--- web/src/components/Form/Input.jsx
+++ web/src/components/Form/Input.jsx
@@ -1,17 +1,17 @@
-export default function Input({ children, label, name, value, onChange, placeholder, type, ...props }) {
+export default function Input({ children, label, name, value, onChange, placeholder, type ="text", ...props }) {
 
     return (
         <div>
             <label htmlFor="email"
-                className="">
+                className=" text-sm font-semibold text-blue-950 mb-2 block">
             {label}
             </label>
-            <input type="text"
+            <input type={type}
                 name={name}
                 value={value}
                 onChange={onChange}
                 placeholder={placeholder}
-                className="bg-[#F9FEFF] h-12.5 w-full pl-2 py-3 pr-4 border-2 border-l-0 border-blue-700 focus:outline-none placeholder:text-blue-800 placeholder:text-[14px] placeholder:font-semibold active:bg-blue-50"
+                className="bg-[#F9FEFF] h-12.5 w-full pl-2 py-3 pr-4 border-2 border-blue-700 rounded -lg focus:outline-none placeholder:text-blue-800 placeholder:text-[14px] placeholder:font-semibold active:bg-blue-50"
             />
         </div>
     )
