b3fe3809b6dc7a096c06884591f3c3f40d8850c3
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-03 15:44:05 -0300

    feat: refatorar layout da página inicial para melhor organização e responsividade

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -5,9 +5,12 @@ import Aside from "../components/NavBar/Aside"
 export default function Home(){
 
     return(
-        <main className="w-full min-h-screen flex flex-row">
-        <Aside />
-        <section className="w-full py-20 px-9"><Outlet/></section>
+        <main className="grid grid-cols-[min(260px)_minmax(0,1fr)] grid-rows-[min(50px)_minmax(0,1fr)] w-full min-h-screen">
+        <Aside className={"col-span-1"} />
+        <header className="w-full bg-blue-700 px-16 py-4 text-[#FFF] text-[16px] font-semibold row-span-1 flex items-center justify-start">
+            <span>Unidade de Vigilância em Zoonoses</span>
+            </header>
+        <section className="w-full py-6 px-9 col-start-2"><Outlet/></section>
         </main>
     )
 }
