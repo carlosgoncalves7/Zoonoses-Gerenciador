0a3a248905c7d82d50bc684e952062b89245b465
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-12 07:09:27 -0300

    feat: adicionar cabeçalhos CORS e configuração de conteúdo para UTF-8

--- server/headers.php
+++ server/headers.php
@@ -6,3 +6,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+
+// Configuração correta para API com UTF-8
+// UTF-8 mantenha a escrita viasual de letras com acento, inpedindo transformar em caracters "avulso"
+header("Content-Type: application/json; charset=utf-8");
