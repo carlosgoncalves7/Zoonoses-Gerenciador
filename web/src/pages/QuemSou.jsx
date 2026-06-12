ff1ef56960cd5ba9208f3fbb0887d6953f9ae362
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-12 07:10:30 -0300

    feat: adicionar componente QuemSou com informações pessoais

---
+++ web/src/pages/QuemSou.jsx
@@ -0,0 +1,13 @@
+
+
+export default function QuemSou() {
+
+    return (
+        <>
+            <header className="w-full flex flex-col justify-center items-start px-3 py-2 mb-3">
+                <span className="text-xl text-blue-950 font-bold">Nome: Carlos Alessandro Ferreira Gonçalves</span>
+                <span className="text-xl text-blue-950 font-bold">RA: 222321</span>
+            </header>
+        </>
+    )
+}
