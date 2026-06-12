18270ae79d471db56d40f6a817eaae0df385da96
Carlos Gonçalves <carlos.unig7@gmail.com>
2026-06-12 07:09:47 -0300

    feat: adicionar endpoints para contagem de animais, gatos, cachorros e procedimentos

---
+++ server/catviews.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once("./headers.php");
+require_once("./config.database.php");
+require_once("./class/DB.php");
+
+// $input = file_get_contents('php://input');
+// $usuario = json_decode($input, true);
+$DSN = $DB_HOST . $DB_PORT . $DB_NAME;
+$db = new DB($DSN, $DB_USER, $DB_PASS);
+
+$cmd = $db->getPDO();
+
+$sql = "SELECT count(*) as total FROM animais where especie = 'Gato'";
+
+$stm = $cmd->prepare($sql);
+$stm->execute();
+
+$response = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+// empty - verifica se esta variavel esta vazia ou não
+// verdadeiro - se estiver vazia 
+// falso - se estiver preenchida
+if (!empty($response)) {
+    echo json_encode($response);
+} else {
+    echo json_encode([
+        "message" => "Não foi encontrado resultados"
+    ]);
+}
